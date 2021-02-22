b8d7bdc8387dc830672d7018b2a2b0d60fd528bb
JonathanTesfaye <jonathan.tesfaye@ocsbstudent.ca>
2021-02-22 02:43:00 +0000

    code changes

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 //The radius variable is given here
 let r = 0
-document.getElementById('input').addEventListener('click', calculate)
+let answer = 0
+document.getElementById('input').addEventListener('click', comparison)
 //A function is created to calculate area or circumference
-function calculate () {
-  document.getElementById('input').value
-  calculate = parseInt(calculate)
+function comparison () {
+  r = document.getElementById('input').value
+  comparison = parseInt(comparison)
+  r = parseInt(r)
   //If statements were created to distinguise area and circumference
   if(document.getElementById('button1').onclick) {
   answer = r * r * Math.Pi
+  document.getElementById('answers').innerHTML = answer
   } else if(document.getElementById('button2').onclick) {
   answer = 2 * Math.Pi * r
+  document.getElementById('answers').innerHTML = answer
+  } else {
+  answer = 12
+  document.getElementById('answers').innerHTML = answer
   }
 }
